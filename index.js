81623b6239d4d84ad7c281cade3689e58e0ab2d1
ng911datasync <dylan@olingeographics.com>
2020-07-26 16:15:30 -0600

    updated the contact page

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-var form = document.getElementById("email-form");
-function handleForm(event) { 
-  event.preventDefault(); 
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST","https://ng911-emailer.herokuapp.com/sendEmail", true);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify({
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message:document.getElementById("message").value
-  }));
-} 
-form.addEventListener('submit', handleForm);
+$(document).ready(function(){
+  $('.parallax').parallax();
+
+  $(document).on('change', '.checkInput', function(event){
+    let name = $('#name').val();
+    let email = $('#email').val();
+    let message = $('#message').val();
+
+    if(name !== '' && email !== ''){
+      $('#submit').attr('disabled', false);
+    }
+    else if(name === '' || email === ''){
+      $('#submit').attr('disabled', true);        
+    }
+  });
+
+  $(document).on('click', '#submit', function(event){
+    event.preventDefault();
+    let name = $('#name').val();
+    let email = $('#email').val();
+    let message = $('#message').val();
+    
+    $.post('https://ng911-emailer.herokuapp.com/sendEmail', {
+      name: name,
+      email: email,
+      message: message
+    }).done((data)=>{
+      console.log(data);
+      if(data.success){
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+        Materialize.toast('Email is on its way!', 8000);
+      }
+    });
+  });
+});
